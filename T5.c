6f5978ff64656e954834abb412aa5bf3b7e4c6b8
JotapeSch <jpschons122@gmail.com>
2025-09-18 21:13:31 -0400

    Comentarios

--- T5.c
+++ T5.c
@@ -7,34 +7,109 @@
 #define MAX_LINHAS 500
 
 //------------- CHAMANDO FUNCOES ------------- 
-void formatarTexto(){} 
-void ImprimirFormatado(){}
-void BuscarPlavra(){}
-void SubPrimOcorrencia(){}
-void SubTodasOcorrencia(){}
-void CaixaAlta(){}
-void CaixaBaixa(){}
-void PrimLetraMaiuFrase(){}
-void AlinharEsquerda(){}
-void AlinharDireita(){}
-void Justificar(){}
-void Centralizar(){}
-//---------------------------------------------
+void formatarTexto(int *qtd_linhas, char *text, char linhas[MAX_LINHAS][MAX_LINHA_compr+1]); 
+void ImprimirFormatado(int qtd_linhas, char linhas[MAX_LINHAS][MAX_LINHA_compr+1]);
+void BuscarPlavra(char linhas[MAX_LINHAS][MAX_LINHA_compr + 1], int qtd_linhas);
+void SubPrimOcorrencia(char *text);
+void SubTodasOcorrencia(char *text);
+void CaixaAlta(char *text);
+void CaixaBaixa(char *text);
+void PrimLetraMaiuFrase(char *text);
+void AlinharEsquerda(int qtd_linhas, char linhas[MAX_LINHAS][MAX_LINHA_compr+1]);
+void AlinharDireita(int qtd_linhas, char linhas[MAX_LINHAS][MAX_LINHA_compr+1]);
+void Justificar(char linhas[MAX_LINHAS][MAX_LINHA_compr + 1], int qtd_linhas);
+void Centralizar(char linhas[MAX_LINHAS][MAX_LINHA_compr + 1], int qtd_linhas);
 
 
-void formatarTexto(int *qtd_linhas, char *text, char linhas[MAX_LINHAS][MAX_LINHA_compr+1]){} //JOTA
-void ImprimirFormatado(int qtd_linhas, char linhas[MAX_LINHAS][MAX_LINHA_compr+1]){} //JOTA
-void BuscarPlavra(char linhas[MAX_LINHAS][MAX_LINHA_compr + 1], int qtd_linhas){}//JOTA
-void SubPrimOcorrencia(char *text){}//JOTA
-void SubTodasOcorrencia(char *text){} //RYAN
-void CaixaAlta(char *text){}  //RYAN
-void CaixaBaixa(char *text){} //RYAN
-void PrimLetraMaiuFrase(char *text){} //RYAN
-void AlinharEsquerda(int qtd_linhas, char linhas[MAX_LINHAS][MAX_LINHA_compr+1]){}//VINI
-void AlinharDireita(int qtd_linhas, char linhas[MAX_LINHAS][MAX_LINHA_compr+1]){}//vini
-void Justificar(char linhas[MAX_LINHAS][MAX_LINHA_compr + 1], int qtd_linhas){} //VINI
-void Centralizar(char linhas[MAX_LINHAS][MAX_LINHA_compr + 1], int qtd_linhas){} //vini
+//------------- IMPLEMENTACOES  -------------
+// objetivo: Divide o texto em linhas com no máximo 80 caracteres, preservando palavras
+// parametros: ponteiro para quantidade de linhas, texto original, matriz de linhas para armazenar o texto formatado
+// retorno: nenhum
+void formatarTexto(int *qtd_linhas, char *text, char linhas[MAX_LINHAS][MAX_LINHA_compr+1]){
+    // Implementação pendente - JOTA
+}
 
+// objetivo: Imprime o texto já formatado linha por linha
+// parametros: quantidade de linhas, matriz de linhas contendo o texto formatado
+// retorno: nenhum
+void ImprimirFormatado(int qtd_linhas, char linhas[MAX_LINHAS][MAX_LINHA_compr+1]){
+    // Implementação pendente - JOTA
+}
+
+// objetivo: Procura uma palavra específica no texto e mostra suas ocorrências
+// parametros: matriz de linhas contendo o texto, quantidade de linhas do texto
+// retorno: nenhum
+void BuscarPlavra(char linhas[MAX_LINHAS][MAX_LINHA_compr + 1], int qtd_linhas){
+    // Implementação pendente - JOTA
+}
+
+// objetivo: Substitui apenas a primeira ocorrência de uma palavra no texto
+// parametros: texto original onde será feita a substituição
+// retorno: nenhum
+void SubPrimOcorrencia(char *text){
+    // Implementação pendente - JOTA
+}
+
+// objetivo: Substitui todas as ocorrências de uma palavra no texto
+// parametros: texto original onde serão feitas as substituições
+// retorno: nenhum
+void SubTodasOcorrencia(char *text){
+    // Implementação pendente - RYAN
+}
+
+// objetivo: Converte todo o texto para letras maiúsculas
+// parametros: texto que será convertido para caixa alta
+// retorno: nenhum
+void CaixaAlta(char *text){
+    // Implementação pendente - RYAN
+}
+
+// objetivo: Converte todo o texto para letras minúsculas
+// parametros: texto que será convertido para caixa baixa
+// retorno: nenhum
+void CaixaBaixa(char *text){
+    // Implementação pendente - RYAN
+}
+
+// objetivo: Capitaliza a primeira letra de cada frase no texto
+// parametros: texto que terá as primeiras letras das frases capitalizadas
+// retorno: nenhum
+void PrimLetraMaiuFrase(char *text){
+    // Implementação pendente - RYAN
+}
+
+// objetivo: Alinha todo o texto à esquerda (padrão)
+// parametros: quantidade de linhas, matriz de linhas contendo o texto
+// retorno: nenhum
+void AlinharEsquerda(int qtd_linhas, char linhas[MAX_LINHAS][MAX_LINHA_compr+1]){
+    // Implementação pendente - VINI
+}
+
+// objetivo: Alinha todo o texto à direita
+// parametros: quantidade de linhas, matriz de linhas contendo o texto
+// retorno: nenhum
+void AlinharDireita(int qtd_linhas, char linhas[MAX_LINHAS][MAX_LINHA_compr+1]){
+    // Implementação pendente - VINI
+}
+
+// objetivo: Justifica o texto (distribui espaços uniformemente entre palavras)
+// parametros: matriz de linhas contendo o texto, quantidade de linhas
+// retorno: nenhum
+void Justificar(char linhas[MAX_LINHAS][MAX_LINHA_compr + 1], int qtd_linhas){
+    // Implementação pendente - VINI
+}
+
+// objetivo: Centraliza todas as linhas do texto
+// parametros: matriz de linhas contendo o texto, quantidade de linhas
+// retorno: nenhum
+void Centralizar(char linhas[MAX_LINHAS][MAX_LINHA_compr + 1], int qtd_linhas){
+    // Implementação pendente - VINI
+}
+//-----------------------------------------------------
+
+// objetivo: Exibe menu interativo e chama funções de formatação de texto conforme escolha do usuário
+// parametros: texto original, matriz de linhas formatadas, ponteiro para quantidade de linhas
+// retorno: nenhum
 void Menu(char *texto, char linhas[MAX_LINHAS][MAX_LINHA_compr + 1], int *qtd_linhas){
     int opcao;
     do{
@@ -52,73 +127,109 @@ void Menu(char *texto, char linhas[MAX_LINHAS][MAX_LINHA_compr + 1], int *qtd_li
         printf("11 - Centralizar\n");
         printf("0 - Sair\n");
         printf("Escolha: ");
-        scanf("%d", &opcao);
+        
+        // Validação de entrada
         if (scanf("%d", &opcao) != 1) {
             printf("Entrada invalida. Tente novamente.\n");
             while (getchar() != '\n'); // Limpa o buffer
             continue;
         }
+        
+        // Limpar buffer após scanf
+        while (getchar() != '\n');
+        
         switch (opcao)
         {
         case 1:
-            ImprimirFormatado(qtd_linhas, linhas);
+            // objetivo: Imprime o texto formatado
+           
+            ImprimirFormatado(*qtd_linhas, linhas);
             break;
 
         case 2:
-            BuscarPalavra(linhas, *qtd_linhas);
+            // objetivo: Busca palavra no texto formatado
+            
+            BuscarPlavra(linhas, *qtd_linhas);
             break;
 
         case 3:
+            // objetivo: Substitui primeira ocorrência e reformata texto
+            
             SubPrimOcorrencia(texto);
-            formatarTexto(texto, linhas, qtd_linhas);
+            formatarTexto(qtd_linhas, texto, linhas);
             break;
         
         case 4:
+            // objetivo: Substitui todas ocorrências e reformata texto
+            
             SubTodasOcorrencia(texto);
-            formatarTexto(texto, linhas, qtd_linhas);
+            formatarTexto(qtd_linhas, texto, linhas);
             break;
 
         case 5:
+            // objetivo: Converte para maiúsculas e reformata texto
+            
             CaixaAlta(texto);
-            formatarTexto(texto, linhas, qtd_linhas);
+            formatarTexto(qtd_linhas, texto, linhas);
             break;
 
         case 6:
+            // objetivo: Converte para minúsculas e reformata texto
+            
             CaixaBaixa(texto);
-            formatarTexto(texto, linhas, qtd_linhas);
+            formatarTexto(qtd_linhas, texto, linhas);
             break;
 
         case 7:
+            // objetivo: Capitaliza frases e reformata texto
+            
             PrimLetraMaiuFrase(texto);
-            formatarTexto(texto, linhas, qtd_linhas);
+            formatarTexto(qtd_linhas, texto, linhas);
             break;
         
         case 8:
-            AlinharEsquerda(qtd_linhas, linhas);
+            // objetivo: Alinha texto à esquerda
+            
+            AlinharEsquerda(*qtd_linhas, linhas);
             break;
 
         case 9:
-            AlinharDireita(qtd_linhas, linhas);
+            // objetivo: Alinha texto à direita
+            
+            AlinharDireita(*qtd_linhas, linhas);
             break;
 
         case 10:
-            Justificar(qtd_linhas, linhas);
+            // objetivo: Justifica o texto
+            
+            Justificar(linhas, *qtd_linhas);
             break;
 
         case 11:
-            Centralizar(qtd_linhas, linhas);
+            // objetivo: Centraliza o texto
+            
+            Centralizar(linhas, *qtd_linhas);
             break;   
             
+        case 0:
+            // objetivo: Sai do programa
+            printf("Saindo do programa...\n");
+            break;
+            
         default:
+            //Trata opção inválida
+            
+            printf("Opção inválida! Tente novamente.\n");
             break;
         }
     } while (opcao != 0);
-
 }
-
 
 int main()
 {
+
+    
+    // Declaração do texto original sobre Bill Gates
     char text[MAX_TEXTO]="William Henry Gates III KBE GCIH (Seattle, 28 de outubro de 1955) mais conhecido como Bill Gates,\
  e um magnata, empresario, diretor executivo, investidor, filantropo e autor americano, que ficou conhecido por \
  fundar junto com Paul Allen a Microsoft a maior e mais conhecida empresa de software do mundo em termos de valor \
@@ -150,18 +261,17 @@ int main()
  o programa e vendeu-o por US$ 8 milhoes, mantendo a licenca do produto. Este viria a ser o MS-DOS. \
  Fonte: https://pt.wikipedia.org/wiki/Bill_Gates";
 
-
-    int qtd_linhas = 0;
-    char linhas[MAX_LINHAS][MAX_LINHA_compr+1];
+    // Variáveis para controle do texto formatado
+    int qtd_linhas = 0;  // quantidade de linhas do texto formatado
+    char linhas[MAX_LINHAS][MAX_LINHA_compr+1];  // matriz para armazenar linhas formatadas
     
-    
-
-    //FORMATAR PELA PRIMEIRA VEZ
+    // FORMATAR PELA PRIMEIRA VEZ
     formatarTexto(&qtd_linhas, text, linhas);
-    //CHAMA MENU
+    
+    // CHAMA MENU
     Menu(text, linhas, &qtd_linhas);
 
-
-    printf("\n\n%s",text);
-    return 0;
+    // Imprime texto original final (para debug/verificação)
+    printf("\n\nTexto original final:\n%s", text);
+    return 0;  // Sucesso na execução
 }
